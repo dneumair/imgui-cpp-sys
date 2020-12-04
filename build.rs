1ec77a4ba657f6dc1eaaaf06f464c6b77fd32e50
David <dn@vestico.de>
2020-12-04 17:50:45 +0100

    it worked!

--- build.rs
+++ build.rs
@@ -1,27 +1,32 @@
-use std::path::Path;
-use std::fs::File;
-use std::io::Write;
 
 fn main() {
+	println!("cargo:rerun-if-changed=wrapper.h");
+
 	let bindings = bindgen::Builder::default()
 		.header("wrapper.h")
+		.raw_line("#![allow(non_upper_case_globals)]")
+		.raw_line("#![allow(non_camel_case_types)]")
+		.raw_line("#![allow(non_snake_case)]")
+		.raw_line("#![allow(clippy::all)]")
 		.prepend_enum_name(false)
 		.clang_args(&["-x", "c++", "-std=c++14"])
 		.generate()
 		.expect("Unable to generate bindings");
+	bindings
+		.write_to_file("src/bindings.rs")
+		.expect("Could not write to src/bindings.rs");
 	
 	
-	let path = Path::new("src/bindings.rs");
-	let mut out_file = File::create(path).expect("Could not create src/bindings.rs");
-	out_file.write("
-	#![allow(non_upper_case_globals)]\n
-	#![allow(non_camel_case_types)]\n
-	#![allow(non_snake_case)]\n
-	".as_bytes()).expect("Could not write to src/bindings.rs");
+	let mut build = cc::Build::new();
+	build.cpp(true);
+	build.define("CIMGUI_NO_EXPORT",None);
+	build.define("IMGUI_DISABLE_WIN32_FUNCTIONS", None);
+	build.define("IMGUI_DISABLE_OSX_FUNCTIONS", None);
 	
-	out_file.write(
-	bindings.to_string().as_bytes()
-	).expect("Could not write to src/bindings.rs");
+	build.file("third-party/imgui/imgui.cpp");
+	build.file("third-party/imgui/imgui_draw.cpp");
+	build.file("third-party/imgui/imgui_widgets.cpp");
+	build.file("third-party/imgui/imgui_demo.cpp");
 	
-	drop(out_file);
+	build.compile("libimgui.a");
 }
